182dcf91429c9edda7ecf4756ed94e2445079725
wang01h2 <1299839398@qq.com>
2023-11-17 21:32:45 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {useMapStore} from "./store/map.ts";
+
+const { map } = useMapStore()
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-yellow-300">sokoban {{ map }}</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/store/tests/map.spec.ts
@@ -0,0 +1,8 @@
+import {describe, expect, it} from "vitest";
+
+describe('map', () => {
+  it('happy path', () => {
+    expect(true).toBe(true)
+  })
+})
+
