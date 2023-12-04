5e9b50b66dd2df5f08839167ad43c80c4a0820e6
wang01h2 <1299839398@qq.com>
2023-12-04 18:04:27 +0800

    fix: build

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-// import Game from "./components/game/Game.vue";
+import Game from "./components/game/Game.vue";
 </script>
 
 <template>
   <div>
-    <h1>test</h1>
-<!--    <Game></Game>-->
+    <Game></Game>
   </div>
 </template>
 
